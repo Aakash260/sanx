463d6f6bfd500f231a498bd401cf653e8d8655dc
akash <akashnirwan26@gmail.com>
2023-12-20 16:48:10 +0530

    LearnGit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+   
+ <div className='font-satisfy text-2xl flex justify-center items-center h-screen'>Change Font with Tailwind </div>
+
+  )
+}
