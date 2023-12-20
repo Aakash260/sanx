9bd361777da3bb5d3c50496951e238d3955e444f
akash <akashnirwan26@gmail.com>
2023-12-20 16:58:23 +0530

    ChangeText

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,12 @@ import Image from 'next/image'
 export default function Home() {
   return (
    
- <div className='font-satisfy text-2xl flex justify-center items-center h-screen'>Change Font with Tailwind </div>
+ <div className='font-satisfy text-2xl flex justify-center items-center h-screen'>
+  Change Font with Tailwind 
+ 
+  <h2>Do Some Changes and Look Into the code</h2>
+  </div>
+
 
   )
 }
